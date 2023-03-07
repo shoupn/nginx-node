52882ccdee3338b21c3e5039ee9278b270fe7b5d
nick shoup <shoupnb@gmail.com>
2023-03-07 09:37:26 -0700

    chore: adding second instance of node app with random id

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,10 +1,9 @@
 const app = require("express")();
-const appid = process.env.APPID;
+const appid = Math.floor(Math.random() * 100);
 
 app.get("/", (req,res) => 
 res.send(`appid: ${appid} home page: says hello!`))
 
 app.get("/app1", (req,res) => 
 res.send(`appid: ${appid} app1 page: says hello!`))
- 
 app.listen(9999, ()=>console.log(`${appid} is listening on 9999`))
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Nginx as reverse proxy and NodeJS app
+
+This is using nginx as a reverse proxy for a basic node application
+
+## Running 
+
+`docker-compose build` to build the images and create containers. 
+`docker-compose up` to start the containers. 
